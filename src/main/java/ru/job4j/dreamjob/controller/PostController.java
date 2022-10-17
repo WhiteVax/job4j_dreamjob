1d29e878cb3d00e82b2f594eadbf553ff66fc857
WhiteVax <WhiteVax@gmail.com>
2022-10-17 14:35:55 +0300

    7. HTML form. Создание вакансии.

--- src/main/java/ru/job4j/dreamjob/controller/PostController.java
+++ src/main/java/ru/job4j/dreamjob/controller/PostController.java
@@ -3,7 +3,10 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.store.PostStore;
+
+import java.time.LocalDate;
 
 @Controller
 public class PostController {
@@ -14,5 +17,12 @@ public class PostController {
     public String post(Model model) {
         model.addAttribute("posts", postStore.findAll());
         return "posts";
+    }
+
+    @GetMapping("/formAddPost")
+    public String addPost(Model model) {
+        model.addAttribute("post", new Post(1, "Java Developer",
+                "Java, Sql, Spring", LocalDate.of(2022, 10, 9)));
+        return "addPost";
     }
 }
