bc2a315175e4a195d39e46086cc27e5f4988ad06
WhiteVax <WhiteVax@gmail.com>
2022-10-28 19:08:28 +0300

    3. Filter.

---
+++ src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
@@ -0,0 +1,33 @@
+package ru.job4j.dreamjob.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class AuthFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request,
+                         ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+        var req = (HttpServletRequest) request;
+        var res = (HttpServletResponse) response;
+        var uri = req.getRequestURI();
+        if (uri.endsWith("/loginPage") || uri.endsWith("/login")
+                || uri.endsWith("/formAddUser") || uri.endsWith("/registration")
+                || uri.endsWith("/success") || uri.endsWith("/fail")
+        ) {
+            chain.doFilter(req, res);
+            return;
+        }
+        if (req.getSession().getAttribute("user") == null) {
+            res.sendRedirect(req.getContextPath() + "/loginPage");
+            return;
+        }
+        chain.doFilter(req, res);
+    }
+}
