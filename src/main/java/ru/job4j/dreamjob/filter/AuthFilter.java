b3e6546ed699ef497a8b77fecc6ca177676ed58e
WhiteVax <WhiteVax@gmail.com>
2022-10-30 21:22:32 +0200

    3. Filter [Fixed].

--- src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
+++ src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
@@ -6,9 +6,20 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Set;
 
 @Component
 public class AuthFilter implements Filter {
+
+    private static final String USER = "user";
+    private static final String LOGIN_PAGE = "/loginPage";
+
+    private static final Set<String> PAGES = Set.of("/loginPage", "/login", "/formAddUser",
+            "/registration", "/success", "/fail");
+
+    private boolean checkUri(String uri) {
+        return PAGES.stream().anyMatch(uri :: endsWith);
+    }
 
     @Override
     public void doFilter(ServletRequest request,
@@ -17,15 +28,12 @@ public class AuthFilter implements Filter {
         var req = (HttpServletRequest) request;
         var res = (HttpServletResponse) response;
         var uri = req.getRequestURI();
-        if (uri.endsWith("/loginPage") || uri.endsWith("/login")
-                || uri.endsWith("/formAddUser") || uri.endsWith("/registration")
-                || uri.endsWith("/success") || uri.endsWith("/fail")
-        ) {
+        if (checkUri(uri)) {
             chain.doFilter(req, res);
             return;
         }
-        if (req.getSession().getAttribute("user") == null) {
-            res.sendRedirect(req.getContextPath() + "/loginPage");
+        if (req.getSession().getAttribute(USER) == null) {
+            res.sendRedirect(req.getContextPath() + LOGIN_PAGE);
             return;
         }
         chain.doFilter(req, res);
